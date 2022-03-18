2ffcf59748f19bbc96e4d2fb699b38d533333e74
Sergio Besser <sergiobesser@hotmail.com>
2022-03-18 18:34:25 -0300

    Se creo y se guardo un inquilino mediante formulario

--- edificio/views.py
+++ edificio/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from edificio.models import Inquilino
 from .forms import ConsorcioFormulario, InquilinoFormulario, PropietarioFormulario
 # Create your views here.
 
@@ -6,7 +8,16 @@ def propietario(request):
     form = PropietarioFormulario
     return render(request, 'edificio/propietario.html',{'form': form})
 
-def inquilino(request):       
+def inquilino(request):
+    if request.method == 'POST':
+        form = InquilinoFormulario(request.POST)
+        
+        if form.is_valid():
+            data = form.cleaned_data
+            inquilino = Inquilino(nombre=data['nombre'], apellido=data['apellido'], email=data['email'], unidad=data['unidad'])
+            inquilino.save()
+            return redirect('index')
+            
     form = InquilinoFormulario()
     return render(request, 'edificio/inquilino.html', {'form': form})
 
